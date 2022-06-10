6514e64cc9b57d602fda2efb529278c7e4fc9d67
Rahul <rahul.pandita41299@gmail.com>
2022-06-10 14:45:33 +0530

    Log out dummy leads hardcoded in leads array onto console

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
-let myLeads = []
+let myLeads = ["www.lead1.com", "www.lead2.com", "www.lead3.com"]
 const inputEl = document.getElementById("input-el")
 const inputButton = document.getElementById("input-button")
 
 
-inputButton.addEventListener("click", function() {
+inputButton.addEventListener("click", function() { //NO NEED FOR LINKING A INPUT FUNCTION TO HTML INPUT BUTTON
     myLeads.push(inputEl.value)
     console.log(myLeads)
 }) 
+
+for (let i = 0; i < myLeads.length; i++) {  //LOG OUT DUMMY LEADS IN LEADS ARRAY ONTO CONSOLE
+    console.log(myLeads[i])
+}
