68000947fcd299240aa8185f17739d2a5a8c4e1f
Rahul <rahul.pandita41299@gmail.com>
2022-06-10 19:34:39 +0530

    Render out dummy leads in leads array onto webpage (not as li elements)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let myLeads = ["www.lead1.com", "www.lead2.com", "www.lead3.com"]
 const inputEl = document.getElementById("input-el")
 const inputButton = document.getElementById("input-button")
-
+let ulElements = document.querySelector("#ul-element") //GRAB LIST FROM HTML DOC
 
 inputButton.addEventListener("click", function() { //NO NEED FOR LINKING A INPUT FUNCTION TO HTML INPUT BUTTON
     myLeads.push(inputEl.value)
@@ -9,5 +9,6 @@ inputButton.addEventListener("click", function() { //NO NEED FOR LINKING A INPUT
 }) 
 
 for (let i = 0; i < myLeads.length; i++) {  //LOG OUT DUMMY LEADS IN LEADS ARRAY ONTO CONSOLE
-    console.log(myLeads[i])
+    ulElements.textContent += myLeads[i] + " "
+    
 }
