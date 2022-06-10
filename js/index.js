ca951d4fc86ad1de367bfd2bb85e661372c210cd
Rahul <rahul.pandita41299@gmail.com>
2022-06-10 20:03:09 +0530

    No more dummy/hardcoded leads, enter leads in input box, save input renders leads entered by user onto webpage as li elements

--- js/index.js
+++ js/index.js
@@ -1,19 +1,32 @@
-let myLeads = ["www.lead1.com", "www.lead2.com", "www.lead3.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputButton = document.getElementById("input-button")
 let ulElements = document.querySelector("#ul-element") //GRAB LIST FROM HTML DOC
 
 inputButton.addEventListener("click", function() { //NO NEED FOR LINKING A INPUT FUNCTION TO HTML INPUT BUTTON
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
 }) 
 
-for (let i = 0; i < myLeads.length; i++) {  //LOG OUT DUMMY LEADS IN LEADS ARRAY ONTO CONSOLE
+function renderLeads() {
+    let listItems = ""
+
+    for (let i = 0; i < myLeads.length; i++) {  //LOG OUT DUMMY LEADS IN LEADS ARRAY ONTO CONSOLE
     // ulElements.innerHTML += "<li>" + myLeads[i] + "</li>"
     // create element
     // set text content using innerHTML
     // appending to unordered list
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulElements.append(li)
+    // const li = document.createElement("li")
+    // li.textContent += myLeads[i]
+    // ulElements.append(li)
+        listItems += "<li>" + myLeads[i] + "</li>" //CREATES ONE LONG STRING THAT HAS <LI>LEADS</LI> ...
+        console.log(listItems)
 }
+
+    ulElements.innerHTML = listItems  //READS THE STRING, INNERHTML READS IT AND RENDERS AS UNORDERED LIST ITEMS BECAUSE OF THE LI TAGS
+
+
+}
+
+
+
