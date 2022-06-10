8d8b03e23e8a4ef386b43dfbc40a65f093ceb1ce
Rahul <rahul.pandita41299@gmail.com>
2022-06-10 19:47:58 +0530

    Use document.createElement to create li elements, add leads to li elements and append to ul

--- js/index.js
+++ js/index.js
@@ -9,6 +9,11 @@ inputButton.addEventListener("click", function() { //NO NEED FOR LINKING A INPUT
 }) 
 
 for (let i = 0; i < myLeads.length; i++) {  //LOG OUT DUMMY LEADS IN LEADS ARRAY ONTO CONSOLE
-    ulElements.textContent += myLeads[i] + " "
-    
+    // ulElements.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // create element
+    // set text content using innerHTML
+    // appending to unordered list
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulElements.append(li)
 }
